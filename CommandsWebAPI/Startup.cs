6ffb881cdc3c1b87032e637a3f64b67c6aa1bb32
Rayokov <elkhouryray@gmail.com>
2020-10-09 17:10:55 +0300

    simple example

--- CommandsWebAPI/Startup.cs
+++ CommandsWebAPI/Startup.cs
@@ -49,6 +49,8 @@ namespace Commander
             });
         }
 
+        //github test
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
